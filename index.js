9463a57344d18acbc25a946daf096a56a2f058ce
hime2828 <ramidisaisuhas@gmail.com>
2025-03-21 12:13:21 +0530

    server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 const port = 3010;
@@ -10,6 +13,13 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port,async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to database")
+    console.log(`Example app listening at http://localhost:${port}`);
+    
+  } catch (error) {
+    console.log("Error connecting to database:",error);
+  }
 });
